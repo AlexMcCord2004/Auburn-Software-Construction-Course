4cb6bb702b916f30666a3824d81adab00be80259
Alex McCord <115519509+AlexMcCord2004@users.noreply.github.com>
2025-03-16 22:12:03 -0500

    Update SeatReservation.java

---
+++ module1/SeatReservation.java
@@ -0,0 +1,46 @@
+private String flightDesignator;
+private java.time.LocalDate flightDate;
+private String firstName;
+private String lastName;
+
+public String getFlightDesignator() {
+    return flightDesignator;
+}
+
+public void setFlightDesignator(String fd) {
+    this.flightDesignator = fd;
+}
+
+public java.time.LocalDate getFlightDate() {
+    return flightDate;
+}
+
+public void setFlightDate(java.time.LocalDate date) {
+    this.flightDate = date;
+}
+
+public String getFirstName() {
+    return firstName;
+}
+
+public void setFirstName(String fn) {
+    this.firstName = fn;
+}
+
+public String getLastName() {
+    return lastName;
+}
+
+public void setLastName(String ln) {
+    this.lastName = ln;
+}
+
+@Override
+public String toString() {
+    return "SeatReservation{" +
+            "flightDesignator='" + flightDesignator + '\'' +
+            ", flightDate=" + flightDate +
+            ", firstName='" + (firstName != null ? firstName : "null") + '\'' +
+            ", lastName='" + (lastName != null ? lastName : "null") + '\'' +
+            '}';
+}
