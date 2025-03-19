6765450510c941b5ec21bc35acd8220a9475bdf6
Alex McCord <115519509+AlexMcCord2004@users.noreply.github.com>
2025-03-19 14:46:20 -0500

    Update SeatReservation.java

--- module1/SeatReservation.java
+++ module1/SeatReservation.java
@@ -11,9 +11,6 @@ public String getFlightDesignator() {
 }
 
 public void setFlightDesignator(String fd) {
-    if (fd == null || fd.length() < 4 || fd.length() > 6) {
-        throw new IllegalArgumentException("Flight designator must be between 4 and 6 characters long and cannot be null.");
-    }
     this.flightDesignator = fd;
 }
 
