a6f3d617511ef0f96a3e0b71c759bd3cad63078b
Alex McCord <armccord04@gmail.com>
2025-03-30 21:09:16 -0500

    Add module3 project to main CPSC2710 repo

---
+++ module3/module1/SeatReservation.java
@@ -0,0 +1,53 @@
+public class SeatReservation {
+    
+private String flightDesignator;
+private java.time.LocalDate flightDate;
+private String firstName;
+private String lastName;
+
+public String getFlightDesignator() {
+    return flightDesignator;
+}
+
+public void setFlightDesignator(String fd) {
+    if (fd == null || fd.length() < 4 || fd.length() > 6) {
+        throw new IllegalArgumentException("Invalid flight designator.");
+    }
+    this.flightDesignator = fd;
+}
+
+
+public java.time.LocalDate getFlightDate() {
+    return flightDate;
+}
+
+public void setFlightDate(java.time.LocalDate date) {
+    this.flightDate = date;
+}
+
+public String getFirstName() {
+    return firstName;
+}
+
+public void setFirstName(String fn) {
+    this.firstName = fn;
+}
+
+public String getLastName() {
+    return lastName;
+}
+
+public void setLastName(String ln) {
+    this.lastName = ln;
+}
+
+@Override
+public String toString() {
+    return "SeatReservation{" +
+            "flightDesignator=" + (flightDesignator != null ? flightDesignator : "null") +
+            ",flightDate=" + (flightDate != null ? flightDate : "null") +
+            ",firstName=" + (firstName != null ? firstName : "null") +
+            ",lastName=" + (lastName != null ? lastName : "null") +
+            '}';
+}
+}
